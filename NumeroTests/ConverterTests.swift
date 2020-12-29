0d5fa6deebb4cb16de328df79f958eab54666812
mayank <mayank@focistrategy.com>
2020-12-29 15:44:25 +0530

    Added .travis.yml

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -1,5 +1,5 @@
 //
-//  ConverterTests.swift
+//  nnn.swift
 //  NumeroTests
 //
 //  Created by Mayank Purwar on 29/12/20.
@@ -7,23 +7,24 @@
 //
 
 import XCTest
+
 @testable import Numero
 
 class ConverterTests: XCTestCase {
-  var converter: Converter!
-
+    var converter: Converter!
+    
     override func setUpWithError() throws {
-    super.setUp()
-    converter = Converter()
-  }
-
+        super.setUp()
+        converter = Converter()
+    }
+    
     override func tearDownWithError() throws {
-    converter = nil
-    super.tearDown()
-  }
-
-  func testConversionForOne() {
-    let result = converter.convert(1)
-    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
-  }
+        converter = nil
+        super.tearDown()
+    }
+    
+    func testConversionForOne() {
+        let result = converter.convert(1)
+        XCTAssertEqual(result, "2I", "Conversion for 1 is incorrect")
+    }
 }
